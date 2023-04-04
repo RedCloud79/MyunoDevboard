c3cd4310b3567b4de0e25f3a61045e9804ad04e3
RedCloud79 <ghddns79@naver.com>
2023-04-04 12:20:47 +0900

    Button

---
+++ Button_ex/Button_ex.ino
@@ -0,0 +1,21 @@
+const int buttonPin = 3;  // or pin 4
+const int ledPin = 13;    
+
+int buttonState = 0;  
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {
+    // turn LED on:
+    digitalWrite(ledPin, HIGH);
+  } else {
+    // turn LED off:
+    digitalWrite(ledPin, LOW);
+  }
+}
