610dc009020fa65b85c5245846554d362c0010b1
RedCloud79 <ghddns79@naver.com>
2023-04-05 11:06:39 +0900

    while 을 활용한 led 반복 깜빡이기

--- Practice_0/Practice_0.ino
+++ Practice_0/Practice_0.ino
@@ -1,4 +1,5 @@
 const int led = 5;
+const int count = 3;
 
 void blink_0(){
   digitalWrite(led, HIGH);
@@ -6,11 +7,24 @@ void blink_0(){
   digitalWrite(led, LOW);
   delay(500);
 }
+void blink_1(int count){
+  while(count > 0){
+    digitalWrite(led, HIGH);
+    delay(500);
+    digitalWrite(led, LOW);
+    delay(500);
+    count--;//count를 줄인다.
+    Serial.print(count);
+    if(count <= 0) break;
+  }
+}
 
 void setup() {
+  Serial.begin(9600);
   pinMode(led, OUTPUT);
+  blink_1(count);
 }
 
 void loop() {
-  blink_0();
+  //blink_1(count);
 }
