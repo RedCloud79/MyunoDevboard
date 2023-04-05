92f44bf422d92120e9392a126383074563f7aed2
RedCloud79 <ghddns79@naver.com>
2023-04-05 12:59:03 +0900

    버튼 누를경우 종료

--- Poten_control_led/Poten_control_led.ino
+++ Poten_control_led/Poten_control_led.ino
@@ -1,5 +1,6 @@
 const int led = 5;
 int sensor = A1;
+const int but = 3;
 
 void blink(){
   digitalWrite(led, HIGH);
@@ -11,11 +12,15 @@ void blink(){
 void setup() {
   Serial.begin(9600);
   pinMode (led, OUTPUT);
+  pinMode (but, INPUT);
 }
 
 void loop() {
   if(analogRead(sensor) >= 100){
     blink();
+    if(digitalRead(but) == HIGH){
+      exit(0); // loop 종료
+    }
   }
   Serial.println(analogRead(sensor));
 }
