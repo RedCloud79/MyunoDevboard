65b99cf355a38a616ae49b4fe4d6211e0966ccb7
RedCloud79 <ghddns79@naver.com>
2023-04-05 10:48:13 +0900

    Update Practice_0.ino

--- Practice_0/Practice_0.ino
+++ Practice_0/Practice_0.ino
@@ -1,12 +1,16 @@
 const int led = 5;
 
+void blink_0(){
+  digitalWrite(led, HIGH);
+  delay(500);
+  digitalWrite(led, LOW);
+  delay(500);
+}
+
 void setup() {
   pinMode(led, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(led, HIGH);
-  delay(500);
-  digitalWrite(led, LOW);
-  delay(500);
+  blink_0();
 }
